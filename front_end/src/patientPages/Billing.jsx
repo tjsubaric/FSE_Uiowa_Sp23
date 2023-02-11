f50b687868eae70154ed9812ab5a39a4cd736d1a
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-02-10 18:07:15 -0600

    linkin patient

---
+++ front_end/src/patientPages/Billing.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import PatientMenu from "../components/PatientMenu";
+
+export default function Billing () {
+  return (
+    <div>
+      <PatientMenu />
+      <h3>Billing Page</h3>
+    </div>
+  );
+};
