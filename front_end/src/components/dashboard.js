8ef4d1cd6e20ab0858d2dbafd1fd59b257920f49
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-02-13 11:01:08 -0600

    sonarlint

--- front_end/src/components/dashboard.js
+++ front_end/src/components/dashboard.js
@@ -6,8 +6,6 @@ const Dashboard = (props) => {
     <nav className={`dashboard-1 ${props.className || ""}`}>
       <div className="rectangle-3">{props.search || "Search"}</div>
       <Link to="/">Home</Link>
-      <Link to="/patient">Patient</Link>
-      <Link to="/doctor">Doctor</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
     </nav>
