cd29e1832bb2a49f55ab36e17bcde05a4f4f2597
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-02-14 08:13:27 -0600

    update to Dr menu

--- front_end/src/components/DoctorMenu.jsx
+++ front_end/src/components/DoctorMenu.jsx
@@ -3,11 +3,9 @@ import Paper from '@mui/material/Paper';
 import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
-//import ListItemText from '@mui/material/ListItemText';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PersonIcon from '@mui/icons-material/Person';
 import MessageIcon from '@mui/icons-material/Message';
-import PhoneIcon from '@mui/icons-material/Phone';
 import SettingsIcon from '@mui/icons-material/Settings';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import Avatar from '@mui/material/Avatar';
@@ -51,12 +49,6 @@ export default function DoctorMenu() {
             <MessageIcon/>
           </ListItemIcon>
           <Link to="/chats">Chats</Link>
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <PhoneIcon/>
-          </ListItemIcon>
-          <Link to="/calls">Calls</Link>
         </MenuItem>
         <MenuItem>
           <ListItemIcon>
--- front_end/src/components/dashboard.js
+++ front_end/src/components/dashboard.js
@@ -8,6 +8,8 @@ const Dashboard = (props) => {
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
+      <Link to="/patient">Patient</Link>
+      <Link to="/doctor">Doctor</Link>
     </nav>
   );
 };
